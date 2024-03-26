0849b64a6d7ad36bb39b00907254a7397cd642da
Kusmuhambetov <164995827+Kusmuhambetov@users.noreply.github.com>
2024-03-26 11:15:09 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Class1
+    {
+
+        public string Name { get; set; }
+        public string SubName { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Show()
+        {
+            return $" Вы допущены к экзамену! Подздравляем! \n Имя {Name}\nФамилия: {SubName}\nДата рождения {DateTime.ToString("dd-MM-yyyy")}";
+        }
+        public void User(string _Name, string _SubName , DateTime _DateTime)
+        {
+
+            Name = _Name;
+            SubName = _SubName;
+            DateTime = _DateTime;
+
+
+        }
+
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        Random rnd = new Random();
+        private void Dopusk_Click(object sender, RoutedEventArgs e)
+        {
+
+            int y = rnd.Next(0, 2);
+            if (y == 0)
+            {
+                Class1 usr = new Class1();
+                DateTime selectedDate = Calendar.SelectedDate ?? DateTime.MinValue;
+                usr.User(Name.Text, SubName.Text, selectedDate);
+                MessageBox.Show(usr.Show());
+            }
+            else
+            {
+                MessageBox.Show("ОТЧИСЛЕН!!!!");
+            }
+        }
+    }
+}
